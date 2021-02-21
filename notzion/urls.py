426c8d1428fc532ed829c234887798ff43e326c8
7ubi <tobi.renner03@gmail.com>
2021-02-21 20:55:34 +0100

    login implementation

--- notzion/urls.py
+++ notzion/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from project.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view),
+    path('login/', login_view),
 ]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'generic/base.html')
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+
+            user = authenticate(request, username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'login/login.html', {'error': 'Username or Password invalid!'})
+    return render(request, 'login/login.html')
